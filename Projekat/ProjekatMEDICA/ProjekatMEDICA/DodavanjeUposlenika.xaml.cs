e8d4b9fb0074d1b32144dd7da8c3fa9e42b58bb0
ajapalak1 <ajapalak1@etf.unsa.ba>
2017-05-18 18:46:08 +0200

    Izmjene

--- Projekat/ProjekatMEDICA/ProjekatMEDICA/DodavanjeUposlenika.xaml.cs
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/DodavanjeUposlenika.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ProjekatMEDICA.ViewModels;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +24,7 @@ namespace ProjekatMEDICA
     /// </summary>
     public sealed partial class DodavanjeUposlenika : Page
     {
+        DodavanjeUposlenikaViewModel viewModel; 
         public DodavanjeUposlenika()
         {
             this.InitializeComponent();
---
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/ViewModels/DodavanjeUposlenikaViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ProjekatMEDICA.ViewModels
+{
+    class DodavanjeUposlenikaViewModel : INotifyPropertyChanged
+    {
+        public INavigate unosUposlnika;
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public DateTime datumRodjenja { get; set; }
+        public string maticni { get; set; }
+        public string strucnSprema { get; set; }
+        public string komentar { get; set; }
+        public ICommand regBtn { get; set; }
+        public ICommand backBtn { get; set; }
+        public ICommand muskoJe { get; set; }
+        public ICommand zenskoJe { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public void pozoviRegistracija()
+        {
+           unosUposlnika.Navigate(typeof(DodavanjeUposlenika));
+        }
+    }
+}
