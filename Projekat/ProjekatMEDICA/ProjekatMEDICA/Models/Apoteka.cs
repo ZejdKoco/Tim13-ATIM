6324891dd588c3fb8fda5de5c6ca67657a415208
ajapalak1 <ajapalak1@etf.unsa.ba>
2017-05-18 23:56:57 +0200

    izmjene

---
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/Models/Apoteka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMEDICA.Models
+{
+    public class Apoteka
+    {
+        public string _naziv { get; set; }
+        public ObservableCollection<Proizvod> Proizvodi { get => proizvodi; set => proizvodi = value; }
+        public ObservableCollection<Uposlenik> Uposlenici { get => uposlenici; set => uposlenici = value; }
+        public ObservableCollection<Kupac> Kupci { get => kupci; set => kupci = value; }
+
+        private ObservableCollection<Proizvod> proizvodi;
+        private ObservableCollection<Uposlenik> uposlenici;
+        private ObservableCollection<Kupac> kupci;
+        Apoteka(string naziv)
+        {
+            this._naziv = naziv;
+            this.proizvodi = new ObservableCollection<Proizvod>();
+            this.kupci = new ObservableCollection<Kupac>();
+            this.uposlenici = new ObservableCollection<Uposlenik>();
+        }
+    }
+}
--- Projekat/ProjekatMEDICA/ProjekatMEDICA/ViewModels/AzuriranjeProizvodaParentViewModel.cs
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/ViewModels/AzuriranjeProizvodaParentViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using Windows.UI.Popups;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using ProjekatMEDICA.Models;
+using System.Collections.ObjectModel;
 
 namespace ProjekatMEDICA.ViewModels
 {
-    class AzuriranjeProizvodaParentViewModel
+    class AzuriranjeProizvodaParentViewModel : INotifyPropertyChanged
     {
+        public string naziv { get; set; }
+        public ICommand pretragaBtn { get; set; }
+        public ObservableCollection<Proizvod> proizvodi { get; set; }
+        public ICommand potvrdiBtn { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public INavigate azuriranjeProizvodaParent;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public void pozivAzuriranjeProizvodaParent()
+        {
+            azuriranjeProizvodaParent.Navigate(typeof(AzurirajProizvodParent));
+        }
     }
 }
