2552ebf601353b72e0556f68001d7174ba01edc1
Zejd Koco <zkoco1@etf.unsa.ba>
2017-05-19 11:50:50 +0200

    Pocetna Forma VM

--- Projekat/ProjekatMEDICA/ProjekatMEDICA/FormOdabirUloge.xaml.cs
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/FormOdabirUloge.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ProjekatMEDICA.ViewModels;
+using System.Windows.Input;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,11 +22,17 @@ namespace ProjekatMEDICA
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class FormOdabirUloge : Page
     {
-        public BlankPage1()
+        public ICommand loginKupac { get; set; }
+        public ICommand loginProdavac { get; set; }
+        public ICommand loginMenadzer { get; set; }
+        public ICommand loginDostavljac { get; set; }
+
+        public FormOdabirUloge()
         {
             this.InitializeComponent();
+            DataContext = new OdabirUlogeViewModel();
         }
     }
 }
---
+++ Projekat/ProjekatMEDICA/ProjekatMEDICA/ViewModels/OdabirUlogeViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ProjekatMEDICA.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjekatMEDICA.ViewModels
+{
+    class OdabirUlogeViewModel
+    {
+        public ICommand loginKupac { get; set; }
+        public ICommand loginProdavac { get; set; }
+        public ICommand loginMenadzer { get; set; }
+        public ICommand loginDostavljac { get; set; }
+        public INavigationService NavigationService { get; set; }
+        public OdabirUlogeViewModel()
+        {
+            NavigationService = new NavigationService();
+            loginKupac = new RelayCommand<object>(loginKupacF, mozeLogKupac);
+            loginProdavac = new RelayCommand<object>(loginProdavacF, mozeLogProdavac);
+            loginMenadzer = new RelayCommand<object>(loginMenadzerF, mozeLogMenadzer);
+            loginDostavljac = new RelayCommand<object>(loginDostavljacF, mozeLogDostavljac);
+        }
+        public bool mozeLogKupac(Object parameter)
+        {
+            return true;
+        }
+        public bool mozeLogProdavac(Object parameter)
+        {
+            return true;
+        }
+        public bool mozeLogMenadzer(Object parameter)
+        {
+            return true;
+        }
+        public bool mozeLogDostavljac(Object parameter)
+        {
+            return true;
+        }
+        public void loginKupacF(Object parameter)
+        {
+            NavigationService.Navigate(typeof(PrijavaKupca));
+        }
+        public void loginProdavacF(Object parameter)
+        {
+            //NavigationService.Navigate(typeof(PrijavaProdavaca)); /dodatno pitati za prijavu prodavaca
+        }
+        public void loginMenadzerF(Object parameter)
+        {
+            NavigationService.Navigate(typeof(PrijavaMenadzera));
+        }
+        public void loginDostavljacF(Object parameter)
+        {
+            //NavigationService.Navigate(typeof(PrijavaDostavljaca)); /dodatno pitati za prijavu dostavljaca
+        }
+    }
+    
+}
